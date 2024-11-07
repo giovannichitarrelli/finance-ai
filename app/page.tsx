85d811bbf99b1999fb7df227cdf5123a2080eccb
Giovanni <giovanni.chitarrelli@gmail.com>
2024-11-07 19:52:16 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
- 
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>pla</h1>
+      <Button>Hello world</Button>
     </div>
   );
 }
